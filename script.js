85bdd2885c1cfbdef2a868c612b7e6de4f6cfa7f
Sands <82897602+Sands-45@users.noreply.github.com>
2021-07-25 23:54:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,102 +1,107 @@
-let transformSetting = document.getElementById("navbar").style.transform = "scale(1, 1)";
-let widthSetting = (document.getElementById("navbar").style.width = "16.66666667%");
+let transformSetting = (document.getElementById("navbar").style.transform =
+  "scale(1, 1)");
+let widthSetting = (document.getElementById("navbar").style.width =
+  "16.66666667%");
 
-function sideBar(){
- if (transformSetting == "scale(1, 1)" && widthSetting == '16.66666667%') {
-    transformSetting = document.getElementById("navbar").style.transform ="scale(0, 1)";;
-    widthSetting = document.getElementById("navbar").style.width ="0%";
-    document.getElementById("navbar").style.opacity="0";
-     } else {
-     transformSetting = (document.getElementById("navbar").style.transform ="scale(1, 1)");
-     widthSetting = (document.getElementById("navbar").style.width ="16.66666667%");
-     document.getElementById("navbar").style.opacity = "1";
-     }
-   }
+function sideBar() {
+  if (transformSetting == "scale(1, 1)" && widthSetting == "16.66666667%") {
+    transformSetting = document.getElementById("navbar").style.transform =
+      "scale(0, 1)";
+    widthSetting = document.getElementById("navbar").style.width = "0%";
+    document.getElementById("navbar").style.opacity = "0";
+  } else {
+    transformSetting = document.getElementById("navbar").style.transform =
+      "scale(1, 1)";
+    widthSetting = document.getElementById("navbar").style.width =
+      "16.66666667%";
+    document.getElementById("navbar").style.opacity = "1";
+  }
+}
 
-   /* Charts or Dashboard Reports */
-   //Projects Chart =======================================================
-   var options = {
-     series: [
-       {
-         name: "Sales",
-         data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5],
-       },
-     ],
-     chart: {
-       height: 350,
-       type: "line",
-       toolbar: {
-         show: false,
-       },
-     },
-     forecastDataPoints: {
-       count: 7,
-     },
-     stroke: {
-       width: 5,
-       curve: "smooth",
-     },
-     xaxis: {
-       type: "datetime",
-       categories: [
-         "1/11/2000",
-         "2/11/2000",
-         "3/11/2000",
-         "4/11/2000",
-         "5/11/2000",
-         "6/11/2000",
-         "7/11/2000",
-         "8/11/2000",
-         "9/11/2000",
-         "10/11/2000",
-         "11/11/2000",
-         "12/11/2000",
-         "1/11/2001",
-         "2/11/2001",
-         "3/11/2001",
-         "4/11/2001",
-         "5/11/2001",
-         "6/11/2001",
-       ],
-       tickAmount: 10,
-       labels: {
-         formatter: function (value, timestamp, opts) {
-           return opts.dateFormatter(new Date(timestamp), "dd MMM");
-         },
-       },
-     },
-     title: {
-       text: "Work Summary",
-       align: "center",
-       style: {
-         fontSize: "16px",
-         color: "#666",
-       },
-     },
-     fill: {
-       type: "gradient",
-       gradient: {
-         shade: "dark",
-         gradientToColors: ["#FDD835"],
-         shadeIntensity: 1,
-         type: "horizontal",
-         opacityFrom: 0.5,
-         opacityTo: 1,
-         stops: [0, 100, 100, 100],
-       },
-     },
-     yaxis: {
-       min: -10,
-       max: 40,
-     },
-     grid: {
-       yaxis: {
-         lines: {
-           show: false,
-         },
-       },
-     },
-   };
+/* Charts or Dashboard Reports */
+//Projects Chart =======================================================
+var options = {
+  series: [
+    {
+      name: "Sales",
+      data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "line",
+    toolbar: {
+      show: false,
+    },
+  },
+  forecastDataPoints: {
+    count: 7,
+  },
+  stroke: {
+    width: 5,
+    curve: "smooth",
+  },
+  xaxis: {
+    type: "datetime",
+    categories: [
+      "1/11/2000",
+      "2/11/2000",
+      "3/11/2000",
+      "4/11/2000",
+      "5/11/2000",
+      "6/11/2000",
+      "7/11/2000",
+      "8/11/2000",
+      "9/11/2000",
+      "10/11/2000",
+      "11/11/2000",
+      "12/11/2000",
+      "1/11/2001",
+      "2/11/2001",
+      "3/11/2001",
+      "4/11/2001",
+      "5/11/2001",
+      "6/11/2001",
+    ],
+    tickAmount: 10,
+    labels: {
+      formatter: function (value, timestamp, opts) {
+        return opts.dateFormatter(new Date(timestamp), "dd MMM");
+      },
+    },
+  },
+  title: {
+    text: "Work Summary",
+    align: "center",
+    style: {
+      fontSize: "16px",
+      color: "#666",
+    },
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      gradientToColors: ["#FDD835"],
+      shadeIntensity: 1,
+      type: "horizontal",
+      opacityFrom: 0.5,
+      opacityTo: 1,
+      stops: [0, 100, 100, 100],
+    },
+  },
+  yaxis: {
+    min: -10,
+    max: 40,
+  },
+  grid: {
+    yaxis: {
+      lines: {
+        show: false,
+      },
+    },
+  },
+};
 var chart = new ApexCharts(document.querySelector("#chartOne"), options);
 chart.render();
 
@@ -115,13 +120,13 @@ function restaurants() {
 }
 function queries() {
   if (document.getElementById("password1").value !== "assurance@123") {
-    document.getElementById("dashboards").src = "";
+    document.getElementById("dashboards6").src = "";
     document.getElementById("response1").innerHTML = "Incorrect password 🤨";
   } else {
-    document.getElementById("dashboards").src =
+    document.getElementById("dashboards6").src =
       "https://datastudio.google.com/embed/reporting/49454e7b-3ef0-4d81-9d68-70a2d118fdf4/page/jKlQC";
     document.getElementById("pass-box1").style.display = "none";
-    document.getElementById("sidenav").style.pointerEvents = "inherit";
+    document.getElementById("navbar").style.pointerEvents = "inherit";
   }
 }
 function warnings() {
